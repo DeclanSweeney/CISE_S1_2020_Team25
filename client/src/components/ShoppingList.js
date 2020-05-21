56180d07ab0ba2f01d0d0bf668ba6dfb68fd74a5
Declan <Declan.SweeneyNZDF@gmail.com>
2020-05-21 18:28:38 +1200

    Delete through redux state manager added

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -3,13 +3,17 @@ import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { v4 as uuid } from "uuid";
 import { connect } from "react-redux";
-import { getItems } from "../actions/itemActions";
+import { getItems, deleteItem } from "../actions/itemActions";
 import PropTypes from "prop-types";
 
 class ShoppingList extends Component {
   componentDidMount() {
     this.props.getItems();
   }
+
+  onDeleteClick = (id) => {
+    this.props.deleteItem(id);
+  };
 
   render() {
     const { items } = this.props.item;
@@ -39,11 +43,7 @@ class ShoppingList extends Component {
                     className="delete-btn"
                     color="danger"
                     size="sm"
-                    onClick={() => {
-                      this.setState((state) => ({
-                        items: state.items.filter((item) => item.id !== id),
-                      }));
-                    }}
+                    onClick={this.onDeleteClick.bind(this, id)}
                   >
                     &times;
                   </Button>
@@ -68,4 +68,4 @@ const mapStateToProps = (state) => ({
   item: state.item,
 });
 
-export default connect(mapStateToProps, { getItems })(ShoppingList);
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
