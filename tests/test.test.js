21140b468176b5e090c4bb1ababc06767e04eb72
Sinan Rassam <sinan.rassam@outlook.com>
2020-05-22 09:17:51 +1200

    Setting up unit tests

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,7 +1,7 @@
 var expect = require("chai").expect;
 
-describe("Playing arround with Tests", function () {
-    it("Used to try and return fails and successes", function () {
+describe("Testing Unit Tests", function () {
+    it("Basic Test", function () {
         expect(1 + 1).to.equal(2);
         expect(2 + 2).to.equal(4);
     });
