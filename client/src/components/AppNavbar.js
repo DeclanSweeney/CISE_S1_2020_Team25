c9d061c3ca5532a3ce8808c5b16ca35ad95573d3
Declan <Declan.SweeneyNZDF@gmail.com>
2020-05-22 08:03:21 +1200

    Add seer logo

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,47 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Container
-} from 'reactstrap';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from "reactstrap";
+import Logo from "./seer_logo.png";
 
 class AppNavbar extends Component {
-    state = {
-        isOpen: false
-    }
+  state = {
+    isOpen: false,
+  };
 
-    toggle = () => {
-        this.state({
-            isOpen: !this.state.isOpen
-        });
-     }
+  toggle = () => {
+    this.state({
+      isOpen: !this.state.isOpen,
+    });
+  };
 
-     render() {
-         return(
-            <div>
-            <Navbar color="dark" dark expand="sm" className="mb-5">
-               <Container>
-                   <NavbarBrand href="/">SEER</NavbarBrand>
-                   <NavbarToggler onClick={this.toggle}/>
-                   <Collapse isOpen={this.state.isOpen} navbar>
-                       <Nav className="ml-auto" navbar>
-                           <NavItem>
-                               <NavLink href="https://github.com/DeclanSweeney/SE-Sem1">
-                                   GitHub
-                               </NavLink>
-                           </NavItem>
-                       </Nav>
-                   </Collapse>
-               </Container>
-            </Navbar>
-        </div>
-         );
-     }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">
+              <img
+                src={Logo}
+                width="180"
+                height="90"
+                className="d-inline-block align-top"
+                alt="SEER Logo"
+              />
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/DeclanSweeney/SE-Sem1">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default AppNavbar;
