a737b3cf13b86da33ad42699c59125ddf5a58bf2
Declan <Declan.SweeneyNZDF@gmail.com>
2020-05-21 21:56:35 +1200

    Predeployment checks complete

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const items = require('./routes/api/items');
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -10,15 +11,26 @@ const app = express();
 app.use(bodyParser.json());
 
 //DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //Connect to Mongo
-mongoose.connect(db)
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 //User Routes
-app.use('/api/items', items);
+app.use("/api/items", items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //Connect to port 5000 unless deployed
 const port = process.env.PORT || 5000;
