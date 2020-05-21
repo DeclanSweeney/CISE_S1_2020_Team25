0bae093c20be74ba43647355141624277a6a6d46
Sinan Rassam <sinan.rassam@outlook.com>
2020-05-22 09:09:32 +1200

    Fixed unit tests

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,6 +1,4 @@
 var expect = require("chai").expect;
-var request = require("request");
-var app = require("../app");
 
 describe("Playing arround with Tests", function () {
     it("Used to try and return fails and successes", function () {
