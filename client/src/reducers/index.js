fa01a0ba1fc3fc0bfa8eeb2b0bdea10633ed85df
Sinan Rassam <sinan.rassam@outlook.com>
2020-06-02 12:44:20 +1200

    Removed previous reference to items

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
-import itemReducer from "./itemReducer";
 import articleReducer from "./articleReducer";
 
 export default combineReducers({
-  item: itemReducer,
   article: articleReducer,
 });
