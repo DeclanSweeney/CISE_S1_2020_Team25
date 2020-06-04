0bb88fd20207a524150a3b71c476fb73d5019b51
Sinan Rassam <sinan.rassam@outlook.com>
2020-06-04 21:20:36 +1200

    Switched back to test db

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://declan:declan123@mern-seer-poe68.mongodb.net/seer?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://declan:declan123@mern-seer-poe68.mongodb.net/test?retryWrites=true&w=majority'
 }
