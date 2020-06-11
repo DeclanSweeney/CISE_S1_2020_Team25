9bce602266375c6815931d60b7ea5d140ffc498b
Sinan Rassam <sinan.rassam@outlook.com>
2020-06-11 20:26:06 +1200

    Switched to using .env to store database link and setup node enviroment to get the correct database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
+require('dotenv').config();
 
 const articles = require("./routes/api/articles");
 
@@ -10,8 +11,20 @@ const app = express();
 //Bodyparser Middleware
 app.use(bodyParser.json());
 
-//DB Config
-const db = require("./config/keys").mongoURI;
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  db = process.env.MONGO_URI_PROD;
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else if (process.env.NODE_ENV === "development") {
+  db = process.env.MONGO_URI_PROD;
+} else {  
+  db = process.env.MONGO_URI_UAT;
+}
 
 //Connect to Mongo
 mongoose
@@ -22,15 +35,7 @@ mongoose
 //User Routes
 app.use("/api/articles", articles);
 
-//Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+var db;
 
 //Connect to port 5000 unless deployed
 const port = process.env.PORT || 5000;
