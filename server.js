1d4b93543d9118174b5b1c6c9c7445ac5d8fd647
Sinan Rassam <sinan.rassam@outlook.com>
2020-06-11 21:26:45 +1200

    Debugging server with travis ci

--- server.js
+++ server.js
@@ -23,8 +23,10 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-} else {  
+} else {
+  console.log("Else");
   db = process.env.MONGO_URI_UAT;
+  console.log("DB: " + db);
 }
 
 //Connect to Mongo
